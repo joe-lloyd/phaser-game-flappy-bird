acac03149f8dfbb98fbcca8391bd82304a8ba301
joe <joe.lloyd@gmail.com>
2020-08-23 19:11:54 +0200

    added debug mode and ts config

--- src/index.ts
+++ src/index.ts
@@ -8,7 +8,8 @@ const config = {
     physics: {
         default: 'arcade',
         arcade: {
-            gravity: { y: 200 }
+            gravity: { y: 200 },
+            debug: true,
         }
     },
     scene: [FlappyGame]
