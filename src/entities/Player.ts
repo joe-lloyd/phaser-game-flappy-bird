b98daadec0c211aeb78005c52e12d5c01bf98cc0
joe <joe.lloyd@gmail.com>
2020-08-26 21:39:54 +0200

    rotate as you go up or fall

--- src/entities/Player.ts
+++ src/entities/Player.ts
@@ -11,6 +11,20 @@ class Player extends Phaser.Physics.Arcade.Sprite {
     return this;
   }
 
+  static preload(scene) {
+    scene.load.image('player', playerAsset);
+  }
+
+  update(pipes, ...args) {
+    super.update(...args);
+    this.checkJump();
+    this.scene.physics.overlap(this, pipes, () => this.hitPipe())
+    this.setFacing()
+  }
+
+  /**
+   * init the player
+   */
   init() {
     this.scene.add.existing(this);
     this.scene.physics.add.existing(this);
@@ -19,6 +33,10 @@ class Player extends Phaser.Physics.Arcade.Sprite {
     this.spaceKey = this.scene.input.keyboard.addKey('SPACE');
   }
 
+  /**
+   * Check if the space key is pressed down.
+   * Makes sure a user cant just hold it down to fly up.
+   */
   checkJump() {
     if (this.canJump && this.spaceKey.isDown) {
         this.setVelocityY(-120);
@@ -28,20 +46,21 @@ class Player extends Phaser.Physics.Arcade.Sprite {
     }
   }
 
+  /**
+   * When hitting a pipe turn off any movement
+   */
   hitPipe() {
     this.body.allowGravity = false;
     this.setVelocityY(0)
     this.setImmovable(true);
   }
 
-  static preload(scene) {
-    scene.load.image('player', playerAsset);
-  }
-
-  update(pipes, ...args) {
-    super.update(...args);
-    this.checkJump();
-    this.scene.physics.overlap(this, pipes, () => this.hitPipe())
+  /**
+   * Face more down as you fall and up as you fly up
+   */
+  setFacing() {
+    const angle = this.angle + this.body.velocity.y * 0.01
+    this.angle = Phaser.Math.Clamp(angle, -90, 90);
   }
 }
 
