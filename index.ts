bc0c7d3ec924133a25edb8845ca6903883583d27
joe <joe.lloyd@gmail.com>
2020-09-20 09:15:27 +0200

    preped package for reuse
    version bump

---
+++ index.ts
@@ -0,0 +1,18 @@
+import GameConfig from './src';
+import FlappyGame from './src/scenes/FlappyGame';
+import Splash from './src/scenes/Splash';
+import GameOver from './src/scenes/GameOver';
+
+/**
+ * export default config for the game
+ */
+export default GameConfig;
+
+/**
+ * export scenes to build your own game
+ */
+export {
+    FlappyGame,
+    Splash,
+    GameOver,
+};
