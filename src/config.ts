cf44ce37773dededaa8bfed5dc12c9a02fd382fa
joe <joe.lloyd@gmail.com>
2020-10-04 10:23:26 +0200

    Fixed config issue for deployment use

--- src/config.ts
+++ src/config.ts
@@ -3,25 +3,26 @@ import Splash from './scenes/Splash';
 import FlappyGame from './scenes/FlappyGame';
 import GameOver from './scenes/GameOver';
 
-const initGame = () => {
-    const config = {
-        type: Phaser.AUTO,
-        width: 800,
-        height: 600,
-        scale: {
-            mode: Phaser.Scale.RESIZE,
-        },
-        physics: {
-            default: 'arcade',
-            arcade: {
-                gravity: { y: 200 },
-                debug: false,
-            }
-        },
-        scene: [Splash, FlappyGame, GameOver]
-    };
+const config = {
+    type: Phaser.AUTO,
+    width: 800,
+    height: 600,
+    scale: {
+        mode: Phaser.Scale.RESIZE,
+    },
+    physics: {
+        default: 'arcade',
+        arcade: {
+            gravity: { y: 200 },
+            debug: false,
+        }
+    },
+    scene: [Splash, FlappyGame, GameOver]
+};
 
+const initGame = () => {
     const game = new Phaser.Game(config);
 }
 
 export default initGame;
+export { config }
--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,5 @@
 import * as Phaser from 'phaser';
-import config from './config';
+import {config} from './config';
 
 const game = new Phaser.Game(config);
 
